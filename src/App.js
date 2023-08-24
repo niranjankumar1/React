89371d89f4cfb29592ffc1f051730fc70ee1ffbe
Sudeep-Poojary <sudeeppoojary0320@gmail.com>
2023-08-24 17:42:52 +0530

    chore: Completed the Application

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Navbar from "./components/Navbar";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchData } from "./actions/action";
+import { useEffect } from "react";
 
 const App = () => {
-  return (
+  const dispatch = useDispatch();
+  const { tickets } = useSelector((state) => state.dataSlice);
+
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
+
+  return tickets ? (
     <div>
       <Navbar />
       <Dashboard />
     </div>
+  ) : (
+    console.error("Something went wrong")
   );
 };
 
